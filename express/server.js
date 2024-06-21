b8d6109c2a9a3754521ba80fd136075022b881d3
Max Steidle <maxsteidleweingard@icloud.com>
2024-06-21 10:28:47 +0200

    Test

--- express/server.js
+++ express/server.js
@@ -2,70 +2,58 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
+// Der port der Schnittstelle
 const port = 3000;
-const Greeting = require('./Greeting.ts'); // Importiere das Greeting-Modell
 
+// Definiert ein Schema für die Begrüßungen
+const greetingSchema = new mongoose.Schema({
+  text: String
+});
+
+// Verbindung mit der Datenbank aufbauen
 mongoose.connect(
-  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/Greetings?authSource=admin`,
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/mongodb-1?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
-)
+);
 
-// Füge einige Begrüßungen zur Datenbank hinzu
+// Erstellt ein Mongoose-Modell basierend auf dem Schema
+const Greeting = mongoose.model('Greetings', greetingSchema);
+
+// Fügt einige Begruessungen zur Datenbank hinzu
 async function addGreetings(){
   try {
-    await Greeting.create({ text: 'Hallo' });
-    await Greeting.create({ text: 'Servus' });
-    await Greeting.create({ text: 'Guten Tag' });
-    await Greeting.create({ text: 'Hallöchen' });
-    await Greeting.create({ text: 'Grüß Gott' });
+    await Greeting.create([
+      { text: 'Hallo' },
+    { text: 'Servus' },
+    { text: 'Guten Tag' },
+    { text: 'Hallöchen' },
+    { text: 'Grüß Gott' }]);
     console.log('Begrüßungen wurden erfolgreich hinzugefügt');
   } catch (err) {
     console.error('Fehler beim Hinzufügen von Begrüßungen:', err);
   }
 };
 
-// Funktion zum Abrufen eines zufälligen Eintrags aus der Datenbank
-async function getRandomGreeting () {
-    try {
-      const count = 5; // Anzahl der Dokumente in der Sammlung ermitteln
-      const randomIndex = Math.floor(Math.random() * count); // Zufälligen Index generieren
-      const randomGreeting = await Greeting.findOne().skip(randomIndex); // Zufälligen Eintrag abrufen
-      return randomGreeting;
-    } catch (err) {
-      console.error('Fehler beim Abrufen einer zufälligen Begrüßung:', err);
-    }
-};
-
+addGreetings();
 
 app.use(cors());
-// let greetings = ['Hallo', 'Servus', 'Guten Tag', 'Hallöchen', 'Grüß Gott'];
 
 // Definiere eine Routenbehandlung für die Wurzel-URL
 app.get('/get', async (req, res) => {
-    // Verbindung zur MongoDB-Datenbank herstellen
-    // mongoose.connect('mongodb://mongodb:27017/greedings-mongo', {
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true,
-    // })
-    // .then(() => {
-    //     console.log('Erfolgreich mit der MongoDB-Datenbank verbunden');
-    //     if(!oncesDone){
-    //         addGreetings(); // Füge Begrüßungen zur Datenbank hinzu
-    //         oncesDone = true;
-    //     }
-    //     greeting = getRandomGreeting(); // Rufe die Funktion zum Abrufen eines zufälligen Eintrags auf, nachdem die Verbindung hergestellt wurde
-    // })
-    // .catch((err) => {
-    //     console.error('Fehler beim Verbinden mit der MongoDB-Datenbank:', err);
-    // });
-    let greeting = getRandomGreeting();
-    res.send(greeting);
+  // zufaellige Begruessung finden und der Schnittstelle zur verfuegung stellen
+  try {
+    const randomIndex = Math.floor(Math.random() * 5); // Zufälligen Index generieren
+    const randomGreeting = await Greeting.find(); // Zufälligen Eintrag abrufen
+    res.send(randomGreeting[randomIndex].text);
+  } catch (err) {
+    console.error('Fehler beim Abrufen einer zufälligen Begrüßung:', err);
+  }
 });
 
 // Starte den Server und lausche auf dem angegebenen Port
 app.listen(port, () => {
-    console.log(`Server läuft auf http://localhost:${port}`);
+    console.log(`Server läuft auf http://localhost:${port}/`);
 });
